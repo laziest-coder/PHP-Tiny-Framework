0f38fb738f49857b14a1340d4143459df3938c87
Jasur 98 <jasurbekyoldoshev98@gmail.com>
2019-03-05 18:19:58 +0500

    doctrine orm configured

---
+++ app/Models/Task.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * @Entity @Table(name="tasks")
+ **/
+
+class Task
+{
+    /** @Id @Column(type="integer") @GeneratedValue **/
+    protected $id;
+
+    /** @Column(type="string") **/
+    protected $body;
+
+    /** @Column(type="string") **/
+    protected $email;
+
+    /** @Column(type="string") **/
+    protected $image;
+
+    /** @Column(type="string") **/
+    protected $username;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    public function setBody($body)
+    {   
+        $this->body = $body;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {   
+        $this->email = $email;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    public function setImage($image)
+    {   
+        $this->image = $image;
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function setUsername($username)
+    {   
+        $this->username = $username;
+    }
+
+}
---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/* Database connection credentials */
+$dbParams = array(
+    'driver'   => 'pdo_mysql',
+    'host' => 'localhost',
+    'user'     => 'root',
+    'password' => 'mysql',
+    'dbname'   => 'express',
+);
+
+
+/* Set this to true if project is under development  */
+$isDevMode = true;
