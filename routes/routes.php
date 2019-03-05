85c02706971303bdda2078eb9775f1c2f07ef272
Jasur 98 <jasurbekyoldoshev98@gmail.com>
2019-03-05 18:07:40 +0500

    added routing system

---
+++ routes/routes.php
@@ -0,0 +1,15 @@
+<?php
+
+use Phroute\Phroute\RouteCollector;
+
+$router = new RouteCollector(); 
+
+$router->controller('/controller','Controllers\\IndexController');
+
+$router->get('hello', function(){ 
+    return 'Hello, PHRoute!';   
+});
+
+$router->get('jasur',function(){
+    return "Hello Jasur :D";
+});
