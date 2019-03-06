074b74603a36cf02525f92a8ce992e845c263940
Jasur 98 <jasurbekyoldoshev98@gmail.com>
2019-03-06 23:58:15 +0500

    dayside added

--- routes/routes.php
+++ routes/routes.php
@@ -4,6 +4,9 @@ use Phroute\Phroute\RouteCollector;
 
 $router = new RouteCollector(); 
 
+$router->get('/dayside', function(){
+    require_once __DIR__. '/../dayside/index.php';
+});
 $router->controller('/controller','Controllers\\IndexController');
 $router->controller('login','Controllers\\UserController');
 $router->controller('task','Controllers\\TaskController');
