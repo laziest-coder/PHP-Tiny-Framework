ebf8c0ea2685666836e17655196925b4480d7a97
Jasur 98 <jasurbekyoldoshev98@gmail.com>
2019-03-06 23:46:18 +0500

    all views finished

--- views/index.blade.php
+++ views/index.blade.php
@@ -1,16 +1,22 @@
 @extends('layout')
 
 @section('content')
-    <div class="col-md-12 el">
-        <div class="col-md-6">
-            <img src="uploads/">
-        </div>
-            <div class="col-md-2">
-                <a href="/task/delete/id/">Delete</a>
+    @foreach($tasks as $task)
+        <div class="col-md-12 el">
+            <div class="col-md-6">
+                <img src="uploads/{{ $task->getImage() }}">
             </div>
-        <div class="col-md-4">
-            <p>Email: </p>
-            <p>Text: </p>
+                <div class="col-md-2">
+                @if( $_SESSION['auth'] == 1 )
+                    <a href="/task/delete/{{ $task->getId() }}">Delete</a>
+                @endif
+                </div>
+            <div class="col-md-4">
+                <p>Email: {{ $task->getEmail() }}</p>
+                <p>Username: {{ $task->getUsername() }}</p>
+                <p>Text: {{ $task->getBody() }}</p>
+            </div>
         </div>
-    </div>
+    @endforeach
+    {!! $paginator !!}
 @stop
